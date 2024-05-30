1e1c47b0d212a06eb5c9a4276f160e8daa2c5465
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2024-05-30 10:40:40 -0300

    componentes basicos do front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+          { path: '', redirectTo: 'login', pathMatch: 'prefix' },
+          {
+            path: 'login',
+            title: 'Login',
+            loadComponent: () =>
+              import('./login/login.component').then(
+                (c) => c.LoginComponent
+              ),
+          },
+          {
+            path: 'usuario',
+            title: 'Usuários',
+            loadChildren: () =>
+              import('./usuario/usuario.routes').then(
+                (c) => c.default
+              ),
+          }
+        ]},
+];
---
+++ src/app/usuario/usuario.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { UsuarioComponent } from './usuario.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+
+export default [
+  {
+    path: '',
+    component: UsuarioComponent,
+    children: [
+      {
+        path: '',
+        title: 'Usuario',
+        component: ListagemComponent,
+      },
+      {
+        path: 'cadastrar',
+        title: 'Cadastrar Usuario',
+        component: FormularioComponent,
+      },
+    ],
+  },
+] as Routes;
